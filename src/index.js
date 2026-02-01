19db43b528d95dbce8a37bf81e558c9966d5ff14
stabilenr <stabilenr26@gmail.com>
2026-02-01 13:58:36 -0800

    Added basic form handling and added Typewritter plugin.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  let poemFormElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings:
+      "That it will never come again<br>Is what makes life so sweet.<br>Believing what we do not believe<br>Does not exhilarate.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
